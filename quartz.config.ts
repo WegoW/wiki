d2c46273f4229b5aa823f3c27e727aa09575faaf
root <root@Hermes.home>
2026-04-28 09:42:45 +0200

    Sync concert wiki content 2026-04-28

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Konzertsammlung – Walter Wego",
+    pageTitle: "Konzertliste",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
