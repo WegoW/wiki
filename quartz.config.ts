ddbd5e9d22439aa75323fec16db16ef3903ceef5
root <root@Hermes.home>
2026-04-30 12:28:51 +0200

    Sync concert wiki content 2026-04-30

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Konzertliste",
+    pageTitle: "Konzerte & Filme/Serien",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
